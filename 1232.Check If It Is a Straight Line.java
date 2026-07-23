02d2262006b0585925fd34e2da61ca86f72faa86
miruthnee07 <miru200709@gmail.com>
2026-07-23 13:22:10 +0530

    Add solution to check if coordinates are in a straight line
    Implement a method to check if points form a straight line.

---
+++ 1232.Check If It Is a Straight Line.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkStraightLine(int[][] coordinates) {
+        if(coordinates.length<=2){
+            return true;
+        }
+        int x1= coordinates[0][0],y1=coordinates[1][0],x2=coordinates[1][0],y2=coordinates[1][1];
+        int dx=x2-x1, dy=y2-y1;
+        for (int i=2;i<coordinates.length;i++){
+            int x=coordinates[i][0],y=coordinates[i][1];
+            if((x-x1)*dy!=(y-y1)*dx){
+                return false;
+            }
+        }
+        return true;
+    }
+}
