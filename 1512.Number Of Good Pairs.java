f1188a1ef460c14d21c2902087529a95dc49590d
miruthnee07 <miru200709@gmail.com>
2026-07-27 14:53:25 +0530

    Fix return value to count identical pairs

---
+++ 1512.Number Of Good Pairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int[] count = new int[101];
+        int pairs = 0;
+
+        for (int num : nums) {
+            pairs += count[num];
+            count[num]++;
+        }
+
+        return pairs;
+    }
+}
